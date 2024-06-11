e0fece5cecab82863f7ab410a3c0322b6bca4959
Roger <rogerszwaja23@proton.me>
2024-06-11 20:28:00 +0200

    dodanie zmian w adminie

--- c_serwis/admin.py
+++ c_serwis/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import CreateUserModel, CreateWydarzenieModel
 
+admin.site.register(CreateUserModel)
+admin.site.register(CreateWydarzenieModel)
 # Register your models here.
