20e296044fee265a3329a748e9dd41d2a1de578e
Roger <rogerszwaja23@proton.me>
2024-06-12 19:29:42 +0200

    dodanie edycji uzytkownika i odzyskiwania hasel

--- Serwis_W/urls.py
+++ Serwis_W/urls.py
@@ -24,9 +24,28 @@ from c_serwis.views import hello, SignUpView, create_event, ListViewW
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello', hello ,name='event_list'),
-    path('signup', SignUpView.as_view(), name='signup'),
-    path('accounts/login/' , auth_views.LoginView.as_view() , name='login'),
-    path('accounts/logout/' , auth_views.LogoutView.as_view() , name='logout'),
     path('create', create_event, name='create'),
-    path('list', ListViewW.as_view(), name='list')
+    path('list', ListViewW.as_view(), name='list'),
+
+
+#Obsluga uzytkownika
+    path('signup', SignUpView.as_view(), name='signup'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+     path('accounts/password_change/', auth_views.PasswordChangeView.as_view(),
+    name='password_change'),
+     path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(),
+    name='password_change_done'),
+     path('accounts/password_reset/', auth_views.PasswordResetView.as_view(),
+    name='password_reset'),
+     path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(),
+    name='password_reset_done'),
+     path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+    name='password_reset_confirm'),
+     path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(),
+    name='password_reset_complete')
+
+
 ]
